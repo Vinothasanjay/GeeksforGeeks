ed5c78a45e036ae7c2cd25c935fe017556570c21
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-11 18:54:20 +0530

    Update and rename Check for binary to Check for binary.java

--- Check for binary.java
+++ Check for binary.java
@@ -1,9 +1,9 @@
-def isBinary(string):
+ /*def isBinary(string):
     for char in string:
         if char != '0' and char != '1':
             return 0
     return 1
-Remove whitespace in java
+Remove whitespace in java */
 class Solution
 {
    
