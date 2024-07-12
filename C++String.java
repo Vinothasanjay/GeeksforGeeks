3b0774ddcaa253e35044c5ad33f880db6e8c8ed4
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-12 21:16:25 +0530

    Update C++String.java

--- C++String.java
+++ C++String.java
@@ -6,3 +6,10 @@ class Sol
        return s3;
    }
 }
+class Solution {
+    static String conRevstr(String S1, String S2) {
+        String s3=S1+S2;
+        StringBuilder str=new StringBuilder(s3);
+        return str.reverse().toString();
+    }
+}
