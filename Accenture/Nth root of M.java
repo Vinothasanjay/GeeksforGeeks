6395ec973d928ba8ec44b0871785dbba0128a924
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-31 18:29:57 +0530

    Create Nth root of M.java

---
+++ Accenture/Nth root of M.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public int NthRoot(int n, int m)
+    {
+         for(int i=1;i<=m;i++){
+
+        double cal = Math.pow(i,n);
+
+        if(cal == m){
+
+            return i;
+
+        }
+
+        else if(cal>m){
+
+            break;
+
+        }
+
+    }
+
+    return -1;
+    }
+}
+//if n=2,m=9 then first it find the power upto 9 and if matches return i else -1;
