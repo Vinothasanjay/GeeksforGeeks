7e7c3b28779cddb9d70848b551f904339bd3504d
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-08-01 18:51:35 +0530

    Create Modular node.java

---
+++ Linked List/Modular node.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int modularNode(Node head, int k) {
+        ArrayList<Integer> a=new ArrayList<>();
+        if(head==null){
+            return -1;
+        }
+        Node temp=head;
+        int m=-1;
+        int i=1;
+        while(temp!=null){
+            if(i%k==0){
+                m=temp.data;
+            }
+            temp=temp.next;
+            i++;
+        }
+        return m;
+}
+}
