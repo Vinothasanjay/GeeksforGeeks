5b94446bc5144877bb7473e16ee1999e455d2d71
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-08-06 18:35:01 +0530

    Update Missing number.java

--- Missing number.java
+++ Missing number.java
@@ -12,3 +12,22 @@ class Solution {
     }
 
 }
+another
+class Solution {
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, int arr[]) {
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 2; i++) {
+            if (arr[i + 1] - arr[i] != 1) {
+                return arr[i] + 1;
+            }
+        }
+       
+        if (arr[0] != 1) {
+            return 1;
+        }
+        
+        return arr[n - 2] + 1;
+    }
+    }
