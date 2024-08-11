6650bb8663ca329d01485a9df98699ed02cbdd34
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-08-11 18:00:51 +0530

    Create Non repeating element.java

---
+++ Non repeating element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstNonRepeating(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+       for (int i : arr) {
+            if (map.get(i) == 1) {
+                return i;
+            }
+        }
+        return 0;
+}
+}
